a4b46467b3593728ce029ee36c38bd51e056884f
charles <carlosguerrero0705@gmail.com>
2024-12-26 08:27:21 -0500

    fix: Backend connection

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8000',
+        target: 'https://imgclassifier-774604876933.us-east1.run.app',
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ''),
       },
